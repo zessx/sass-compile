96343377f633305eaf5153b5dfcad848558af9a7
Samuel Marchal <contact@zes.sx>
2015-01-28 15:09:27 +0100

    Update sass-compile.sh

--- sass-compile.sh
+++ sass-compile.sh
@@ -3,19 +3,17 @@
 # $1: input
 # $2: output
 # $3: -d | --debug (optional)
-sass-compile() {
-  INPUT=$1
-  OUTPUT=$2
+INPUT=$1
+OUTPUT=$2
 
-  OPTIONS="--update --stop-on-error --no-cache --sourcemap=none"
-  if [[ -n $3 ]] 
+OPTIONS="--update --stop-on-error --no-cache --sourcemap=none"
+if [[ -n $3 ]] 
+then
+  if [[ $3 == "-d" ]] || [[ $3 == "--debug" ]]
   then
-    if [[ $3 == "-d" ]] || [[ $3 == "--debug" ]]
-    then
-      OPTIONS="${OPTIONS} --trace --line-numbers"
-    fi
+    OPTIONS="${OPTIONS} --trace --line-numbers"
   fi
+fi
 
-  sass ${OPTIONS} ${INPUT}:${OUTPUT}
-  sass ${OPTIONS} --style compressed ${INPUT}:`echo ${OUTPUT} | sed 's/\.css$/.min.css/'`
-}
+sass ${OPTIONS} ${INPUT}:${OUTPUT}
+sass ${OPTIONS} --style compressed ${INPUT}:`echo ${OUTPUT} | sed 's/\.css$/.min.css/'`
